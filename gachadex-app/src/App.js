8420ba1703ef8fe923f6b82d2079b399a4e8b51c
ShiroSilhouette <130464250+ShiroSilhouette@users.noreply.github.com>
2023-04-14 11:33:05 +0800

    Update App.js

--- gachadex-app/src/App.js
+++ gachadex-app/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container" style={{ marginTop: '100px' }}>
+      <ul className="grid-item">
+        <li>
+          <img className="square-img" src="/images/fgo.png" alt="square" />
+        </li>
+        <li>
+          <img className="square-img" src="/images/arknights.png" alt="square" />
+        </li>
+      </ul>
+<br></br>
+      <ul className="grid-item">
+        <li>
+          <img className="square-img" src="/images/fgo.png" alt="square" />
+        </li>
+        <li>
+          <img className="square-img" src="/images/arknights.png" alt="square" />
+        </li>
+      </ul>
+
     </div>
   );
 }

4826edc1eb50b5ff2b7ed3b8aad783a28b7dae6a
ShiroSilhouette <130464250+ShiroSilhouette@users.noreply.github.com>
2023-04-17 16:46:14 +0800

    Update App.js

--- gachadex-app/src/App.js
+++ gachadex-app/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 import RoundedContainer from './RoundedContainer';
+import NavigationBar from './navbar';
+
 
 function App() {
   return (
+    <div>
+    <NavigationBar />
+
     <div className="grid-container" style={{ marginTop: '100px' }}>
       <RoundedContainer>
       <ul className="grid-item">
@@ -35,8 +40,26 @@ function App() {
           <img className="square-img" src="/images/arknights.png" alt="square" />
         </li>
       </ul>
-
+      <br></br>
+  <ul className="grid-item">
+        <li>
+          <img className="square-img" src="/images/fgo.png" alt="square" />
+        </li>
+        <li>
+          <img className="square-img" src="/images/arknights.png" alt="square" />
+        </li>
+      </ul>
+      <br></br>
+  <ul className="grid-item">
+        <li>
+          <img className="square-img" src="/images/fgo.png" alt="square" />
+        </li>
+        <li>
+          <img className="square-img" src="/images/arknights.png" alt="square" />
+        </li>
+      </ul>
 </RoundedContainer>
+    </div>
     </div>
   );
 }

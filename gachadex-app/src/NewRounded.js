bfd9ac776747f987b967765023009d66f2b90fb6
ShiroSilhouette <130464250+ShiroSilhouette@users.noreply.github.com>
2023-05-07 00:33:08 +0800

    Add files via upload

---
+++ gachadex-app/src/NewRounded.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function NewRounded({children}) {
+  return (
+    <div className="new-rounded-container">
+      {children}
+    </div>
+  );
+}
+
+export default NewRounded;
---
+++ gachadex-app/src/UpcomingRounded.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function UpcomingRounded({children}) {
+  return (
+    <div className="upcoming-rounded-container">
+      {children}
+    </div>
+  );
+}
+
+export default UpcomingRounded;

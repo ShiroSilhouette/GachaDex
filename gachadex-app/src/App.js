3c29db2b1c16026c74515deaa0077d90d5eac96e
ShiroSilhouette <130464250+ShiroSilhouette@users.noreply.github.com>
2023-05-07 00:30:35 +0800

    Update App.js

--- gachadex-app/src/App.js
+++ gachadex-app/src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import './App.css';
 import RoundedContainer from './RoundedContainer';
 import NavigationBar from './navbar';
+import NewRounded from './NewRounded';
+import UpcomingRounded from './UpcomingRounded';
 
 
 function App() {
   return (
     <div>
     <NavigationBar />
-
-    <div className="grid-container" style={{ marginTop: '100px' }}>
+    
+    <div className="grid-container" style={{ marginTop: '50px' }}>
+     
       <RoundedContainer>
+      <center>Active Gacha</center>
       <ul className="grid-item">
         <li>
           <img className="square-img" src="/images/fgo.png" alt="square" />
@@ -20,7 +24,7 @@ function App() {
         </li>
       </ul>
     
-<br></br>
+
     
       <ul className="grid-item">
         <li>
@@ -30,39 +34,56 @@ function App() {
           <img className="square-img" src="/images/arknights.png" alt="square" />
         </li>
       </ul>
-
-<br></br>
-  <ul className="grid-item">
-        <li>
-          <img className="square-img" src="/images/fgo.png" alt="square" />
-        </li>
-        <li>
-          <img className="square-img" src="/images/arknights.png" alt="square" />
-        </li>
-      </ul>
-
-      <br></br>
-  <ul className="grid-item">
-        <li>
-          <img className="square-img" src="/images/fgo.png" alt="square" />
-        </li>
-        <li>
-          <img className="square-img" src="/images/arknights.png" alt="square" />
-        </li>
-      </ul>
-
-      <br></br>
-  <ul className="grid-item">
-        <li>
-          <img className="square-img" src="/images/fgo.png" alt="square" />
-        </li>
-        <li>
-          <img className="square-img" src="/images/arknights.png" alt="square" />
-        </li>
-      </ul>
+ 
 </RoundedContainer>
+
+<NewRounded>
+    <center>New Gacha</center>
+      <ul>
+        <li>
+        <img className="square-img" src="/images/hsr.png" alt="square" />
+        </li>
+        <li>
+        <img className="square-img" src="/images/hsr.png" alt="square" />
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+        <img className="square-img" src="/images/hsr.png" alt="square" />
+        </li>
+        <li>
+        <img className="square-img" src="/images/hsr.png" alt="square" />
+        </li>
+      </ul>
+</NewRounded>
+
+<UpcomingRounded>
+    <center>Upcoming Gacha</center>
+      <ul>
+        <li>
+        <img className="square-img" src="/images/endfield.jpg" alt="square" />
+        </li>
+        <li>
+        <img className="square-img" src="/images/endfield.jpg" alt="square" />
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+        <img className="square-img" src="/images/endfield.jpg" alt="square" />
+        </li>
+        <li>
+        <img className="square-img" src="/images/endfield.jpg" alt="square" />
+        </li>
+      </ul>
+</UpcomingRounded>
+
     </div>
+
     </div>
+    
+
 
   );
 
